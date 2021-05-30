37d00a450c284e823b620e029ae13fef5b24622b
Emran <emran@xpeedstudio.com>
2021-05-30 23:24:38 +0600

    update code

--- wpmet-edd-affliate-cross-domain.php
+++ wpmet-edd-affliate-cross-domain.php
@@ -4,27 +4,51 @@ Plugin Name: Wpmet edd affliate cross domain
 Plugin URI: #
 Description: Pass affliate ref data to the main site.
 Author: Emran
-Version: 1.0.0
+Version: 1.2.6
 Author URI: wpmet.com
 */
 
-if(session_id() == ''){
-    session_start();
-}
+define('WPMET_EDD_AFF_URL', plugin_dir_url( __FILE__ ));
+//define('WPMET_EDD_AFF_VAR', '1.2.5' . time()); // for testing, caching preventing
+define('WPMET_EDD_AFF_VAR', '1.2.6');
 
-if(!is_admin() && isset($_GET['rui']) && !empty($_GET['rui']) && is_numeric($_GET['rui'])){
-    $_SESSION["_rui"] = $_GET['rui'];
-    add_action( 'wp_footer', 'wpmet_edd_affliate_cross_domain_function' );
-}
+add_action( 'wp_footer', 'wpmet_edd_affliate_cross_domain_function_footer' );
+add_action( 'wp_head', 'wpmet_edd_aff_config_head' );
 
-if(!is_admin() && isset($_SESSION["_rui"])){
-    add_action( 'wp_footer', 'wpmet_edd_affliate_cross_domain_function' );
-}
 
-function wpmet_edd_affliate_cross_domain_function() {
-    $refurl = 'https://account.wpmet.com/affiliate-tracker.php?rui=' . (isset($_SESSION["_rui"]) ? $_SESSION["_rui"] : -1);
-    // echo $refurl;
+function wpmet_edd_aff_config_head() {
+    
+    // these are the base configuration for edd affliate scripts.
     ?>
-        <iframe src="<?php echo $refurl; ?>" style="height:1px; width:1px; border:0; z-index:-999; opacity:0; overflow: hidden"></iframe>
-    <?php
+    <script type='text/javascript'>
+    /* <![CDATA[ */
+    var affwp_scripts = {"ajaxurl":"https:\/\/account.wpmet.com\/wp-admin\/admin-ajax.php"};
+    /* ]]> */
+    </script>
+    <script type="text/javascript">
+        var AFFWP = AFFWP || {};
+        AFFWP.referral_var = 'rui'; // refferal $_GET peram
+        AFFWP.expiration = 30; // how long a refferal data will stay. (days)
+        AFFWP.debug = 0; // debug true or false.
+        AFFWP.wpmet = '<?php echo WPMET_EDD_AFF_VAR; ?>';
+        
+        AFFWP.cookie_domain = 'account.wpmet.com';
+        
+        AFFWP.referral_credit_last = 0;
+    </script>
+    <?php 
 }
+
+
+
+function wpmet_edd_affliate_cross_domain_function_footer() {
+    // these two files are copied from original EDD affliate plugin.
+    wp_enqueue_script('wpmet_edd_aff_cookie', WPMET_EDD_AFF_URL . "assets/js/jquery.cookie.min.js", [], WPMET_EDD_AFF_VAR, true );
+    wp_enqueue_script('wpmet_edd_aff_tracking', WPMET_EDD_AFF_URL . "assets/js/tracking.js", [], WPMET_EDD_AFF_VAR, true ); // it has a little customization (may be!).
+}
+
+add_action('wp_loaded', function(){
+    remove_action( 'wp_head', array( 'InsertConvertFox', 'insert_tracker' ));
+    add_action( 'wp_footer', array( 'InsertConvertFox', 'insert_tracker' ), 5);
+});
+
