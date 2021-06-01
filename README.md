36d163463f0d2fba596ff55bf5733cbd15874907
Emran <emran@xpeedstudio.com>
2021-06-01 11:44:30 +0600

    rename the project to wpmet essential from wpmet-edd-affliate-cross-domain

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# wpmet-edd-affliate-cross-domain
+# wpmet-essential
+# old name was >> wpmet-edd-affliate-cross-domain
+
+Pass affiliate ref data to the main site, pushing campaign coupon, tweaks in ConvertFox Scripts etc
--- wpmet-essential.php
+++ wpmet-essential.php
@@ -1,16 +1,16 @@
 <?php
 /*
-Plugin Name: Wpmet edd affliate cross domain
+Plugin Name: Wpmet Essential
 Plugin URI: #
-Description: Pass affliate ref data to the main site.
+Description: Pass affiliate ref data to the main site, pushing campaign coupon, tweaks in ConvertFox Scripts etc
 Author: Emran
-Version: 1.2.6
+Version: 1.3.0
 Author URI: wpmet.com
 */
 
 define('WPMET_EDD_AFF_URL', plugin_dir_url( __FILE__ ));
 //define('WPMET_EDD_AFF_VAR', '1.2.5' . time()); // for testing, caching preventing
-define('WPMET_EDD_AFF_VAR', '1.2.6');
+define('WPMET_EDD_AFF_VAR', '1.3.0');
 
 add_action( 'wp_footer', 'wpmet_edd_affliate_cross_domain_function_footer' );
 add_action( 'wp_head', 'wpmet_edd_aff_config_head' );
@@ -18,7 +18,7 @@ add_action( 'wp_head', 'wpmet_edd_aff_config_head' );
 
 function wpmet_edd_aff_config_head() {
     
-    // these are the base configuration for edd affliate scripts.
+    // these are the base configuration for edd affiliate scripts.
     ?>
     <script type='text/javascript'>
     /* <![CDATA[ */
@@ -27,8 +27,8 @@ function wpmet_edd_aff_config_head() {
     </script>
     <script type="text/javascript">
         var AFFWP = AFFWP || {};
-        AFFWP.referral_var = 'rui'; // refferal $_GET peram
-        AFFWP.expiration = 30; // how long a refferal data will stay. (days)
+        AFFWP.referral_var = 'rui'; // referral $_GET param
+        AFFWP.expiration = 30; // how long a referral data will stay. (days)
         AFFWP.debug = 0; // debug true or false.
         AFFWP.wpmet = '<?php echo WPMET_EDD_AFF_VAR; ?>';
         
@@ -47,7 +47,24 @@ function wpmet_edd_affliate_cross_domain_function_footer() {
     wp_enqueue_script('wpmet_edd_aff_tracking', WPMET_EDD_AFF_URL . "assets/js/tracking.js", [], WPMET_EDD_AFF_VAR, true ); // it has a little customization (may be!).
 }
 
+function discount_code(){
+    $iframe = '
+    <iframe style="height:0;width:0;opacity:0;overflow:hidden;" src="https://account.wpmet.com/?discount=ONBOARDSPECIAL"></iframe>
+    ';
+
+    if(isset($_COOKIE['wctct63xjdefsaj']) && $_COOKIE['wctct63xjdefsaj'] == 'fired'){
+        echo $iframe;
+    }    
+
+}
+
 add_action('wp_loaded', function(){
+    if(isset($_GET['promo']) & $_GET['promo'] == 'onboard-coupon'){
+        setcookie('wctct63xjdefsaj', 'fired', time() + (3600 * 6), "/");
+    }
+
+    add_action('wp_footer', 'discount_code');
+
     remove_action( 'wp_head', array( 'InsertConvertFox', 'insert_tracker' ));
     add_action( 'wp_footer', array( 'InsertConvertFox', 'insert_tracker' ), 5);
 });
